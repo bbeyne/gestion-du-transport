a8f21066125b789da3bb7f791b332623aedd435d
moussbgf <mboughazif@gmail.com>
2017-06-13 20:55:09 +0200

    Creation classes Reservation & Profile

--- back/src/main/java/dev/entity/Coordonees.java
+++ back/src/main/java/dev/entity/Coordonees.java
@@ -9,9 +9,43 @@ import javax.persistence.Id;
 
 @Entity
 public class Coordonees {
+	
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private LocalDateTime dateCoordonnees;
 	private String coordonnees; // ex: "10.22,11.00"
+	
+	
+	
+	public Coordonees(LocalDateTime dateCoordonnees, String coordonnees) {
+		super();
+		this.dateCoordonnees = dateCoordonnees;
+		this.coordonnees = coordonnees;
+	}
+	public Coordonees() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getDateCoordonnees() {
+		return dateCoordonnees;
+	}
+	public void setDateCoordonnees(LocalDateTime dateCoordonnees) {
+		this.dateCoordonnees = dateCoordonnees;
+	}
+	public String getCoordonnees() {
+		return coordonnees;
+	}
+	public void setCoordonnees(String coordonnees) {
+		this.coordonnees = coordonnees;
+	}
+	
+	
 }
