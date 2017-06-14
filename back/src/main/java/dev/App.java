dfa4c9f9e5367add26f89daa18f8942f8cb51f69
Marc LE BOULCH <marc.le.boulch@hotmail.fr>
2017-06-14 17:07:39 +0200

    US003 - Menu #3 - Modification de l'index.html

--- back/src/main/java/dev/App.java
+++ back/src/main/java/dev/App.java
@@ -1,4 +1,4 @@
-package dev.app;
+package dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
