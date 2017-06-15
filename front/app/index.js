cb8927fffa109ed7524749ad922f8bad3d7c8a0d
Marc LE BOULCH <marc.le.boulch@hotmail.fr>
2017-06-15 11:10:22 +0200

    US003 - Menu #3 - Ajout de window.API_URL (error Sonar) affichage du logo via URL + style

--- front/app/index.js
+++ front/app/index.js
@@ -5,11 +5,11 @@ import { route } from './app.route';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ReservationsComponent } from './reservations/reservations.component';
 
-
 //Création du module app
 angular.module('app', [RouteModule])
-.value( 'API_URL', API_URL)
-.value('API_RESERVATION', API_URL + "collaborateur/reservations" )
+.value( 'API_URL', window.API_URL)
+.value('API_RESERVATION', window.API_URL + "collaborateur/reservations" )
+
 
 
 .component('accueil', AccueilComponent)
