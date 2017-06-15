44a0bb8f821b1b7d667af35b6d8430f10b078068
Estelle PELLE <estellepelle9261@gmail.com>
2017-06-15 11:05:49 +0200

    USGDT004 Liste des réservations covoiturage #4 connexion Spring Angular OK, recup donnees OK

--- front/app/index.js
+++ front/app/index.js
@@ -4,15 +4,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { route } from './app.route';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ReservationsComponent } from './reservations/reservations.component';
+import { ReservationService } from './reservations/reservations.service'
 
 
 //Création du module app
 angular.module('app', [RouteModule])
 .value( 'API_URL', API_URL)
-.value('API_RESERVATION', API_URL + "collaborateur/reservations" )
+.value('API_RESERVATION', API_URL + "/collaborateur/reservations" )
 
 
 .component('accueil', AccueilComponent)
 .component('reservations', ReservationsComponent)
+
+.service('ReservationService', ReservationService)
 
 .config(route);
