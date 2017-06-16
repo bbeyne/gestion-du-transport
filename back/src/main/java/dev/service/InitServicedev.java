8c09721ecd365cb506cd140dea9a9f9c948fcc50
moussbgf DTA <mboughazif@gmail.com>
2017-06-16 10:32:51 +0200

    Modification jeu de données pour historique reservations et annonces

--- back/src/main/java/dev/service/InitServicedev.java
+++ back/src/main/java/dev/service/InitServicedev.java
@@ -59,6 +59,8 @@ public class InitServicedev implements InitService {
 
 		LocalDateTime dateHeureDepart1 = LocalDateTime.of(2017, 5, 4, 12, 30);
 		LocalDateTime dateHeureDepart2 = LocalDateTime.of(2017, 1, 7, 11, 00);
+		LocalDateTime dateHeureDepart3 = LocalDateTime.of(2018, 1, 7, 11, 00);
+		LocalDateTime dateHeureDepart4 = LocalDateTime.of(2017, 12, 7, 9, 00);
 
 		// LocalDate.parse("1993-06-25")
 
@@ -73,16 +75,24 @@ public class InitServicedev implements InitService {
 				adresse1);
 		Annonce annonce3 = new Annonce(dateHeureDepart2, 1, Annonce.STATUT.TERMINE, profilrep.getOne(3), adresse1,
 				adresse2);
+		Annonce annonce4 = new Annonce(dateHeureDepart3, 5, Annonce.STATUT.ANNULE, profilrep.getOne(2), adresse1,
+				adresse2);
+		Annonce annonce5 = new Annonce(dateHeureDepart4, 5, Annonce.STATUT.TERMINE, profilrep.getOne(5), adresse2,
+				adresse1);
 		annonceRepository.save(annonce1);
 		annonceRepository.save(annonce2);
 		annonceRepository.save(annonce3);
+		annonceRepository.save(annonce4);
+		annonceRepository.save(annonce5);
 
 		Reservation reservation1 = new Reservation(Reservation.STATUT.ANNULE, profilrep.getOne(1), annonce3);
 		Reservation reservation2 = new Reservation(Reservation.STATUT.EN_COURS, profilrep.getOne(2), annonce2);
-		Reservation reservation3 = new Reservation(Reservation.STATUT.TERMINE, profilrep.getOne(3), annonce1);
+		Reservation reservation3 = new Reservation(Reservation.STATUT.TERMINE, profilrep.getOne(3), annonce4);
+		Reservation reservation4 = new Reservation(Reservation.STATUT.TERMINE, profilrep.getOne(3), annonce5);
 		reservationRepository.save(reservation1);
 		reservationRepository.save(reservation2);
 		reservationRepository.save(reservation3);
+		reservationRepository.save(reservation4);
 
 		LocalDateTime dateCoordonnees1 = LocalDateTime.of(2017, 5, 4, 12, 00, 55);
 		LocalDateTime dateCoordonnees2 = LocalDateTime.of(2017, 1, 6, 12, 12, 41);
@@ -91,10 +101,10 @@ public class InitServicedev implements InitService {
 		LocalDateTime dateCoordonnees5 = LocalDateTime.of(2017, 5, 1, 4, 45, 5);
 
 		Coordonees coordonnees1 = new Coordonees(dateCoordonnees1, "10.53543,-50.45684");
-		Coordonees coordonnees2 = new Coordonees(dateCoordonnees1, "104.53543,-502.45684");
-		Coordonees coordonnees3 = new Coordonees(dateCoordonnees1, "120.53543,-540.45684");
-		Coordonees coordonnees4 = new Coordonees(dateCoordonnees1, "140.53543,-540.45684");
-		Coordonees coordonnees5 = new Coordonees(dateCoordonnees1, "100.53543,-50.45684");
+		Coordonees coordonnees2 = new Coordonees(dateCoordonnees2, "104.53543,-502.45684");
+		Coordonees coordonnees3 = new Coordonees(dateCoordonnees3, "120.53543,-540.45684");
+		Coordonees coordonnees4 = new Coordonees(dateCoordonnees4, "140.53543,-540.45684");
+		Coordonees coordonnees5 = new Coordonees(dateCoordonnees5, "100.53543,-50.45684");
 		coordonneesRep.save(coordonnees1);
 		coordonneesRep.save(coordonnees2);
 		coordonneesRep.save(coordonnees3);
@@ -103,12 +113,12 @@ public class InitServicedev implements InitService {
 
 		Voiture voiture1 = new Voiture("PP-154-MM", "Renault", "Xantia", Voiture.CATEGORIE.BERLINE_TAILLE_M, 5,
 				Voiture.STATUT.EN_SERVICE, "url image", coordonnees1);
-		Voiture voiture2 = new Voiture("LE-123-EL", "Citroen", "ZX", Voiture.CATEGORIE.CITADINE_POLYVENTES, 6, Voiture.STATUT.HS,
-				"url image", coordonnees2);
+		Voiture voiture2 = new Voiture("LE-123-EL", "Citroen", "ZX", Voiture.CATEGORIE.CITADINE_POLYVENTES, 6,
+				Voiture.STATUT.HS, "url image", coordonnees2);
 		Voiture voiture3 = new Voiture("AV-781-NN", "Ford", "Fiesta", Voiture.CATEGORIE.BERLINE_TAILLE_S, 5,
 				Voiture.STATUT.EN_SERVICE, "url image", coordonnees3);
-		Voiture voiture4 = new Voiture("KF-542-LG", "Citroen", "CX", Voiture.CATEGORIE.MICRO_CITADINE, 6, Voiture.STATUT.HS,
-				"url image", coordonnees4);
+		Voiture voiture4 = new Voiture("KF-542-LG", "Citroen", "CX", Voiture.CATEGORIE.MICRO_CITADINE, 6,
+				Voiture.STATUT.HS, "url image", coordonnees4);
 		voitureRep.save(voiture1);
 		voitureRep.save(voiture2);
 		voitureRep.save(voiture3);
@@ -118,6 +128,9 @@ public class InitServicedev implements InitService {
 		ReservVehicule reserv2 = new ReservVehicule(dateHeureDepart2, dateCoordonnees3, voiture4, profilrep.getOne(2));
 		reservVoitRep.save(reserv1);
 		reservVoitRep.save(reserv2);
-
+		
+		
+		
+		
 	}
 }
